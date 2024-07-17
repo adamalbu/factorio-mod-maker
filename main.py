733baf87eab97fab91c1c327f296c4d88bc888f3
adamalbu <45970222+adamalbu@users.noreply.github.com>
2024-07-17 20:24:16 +0100

    new location ui

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ window = uic.loadUi("ui\\mainwindow.ui")
 
 
 def update_button_text():
-    if config["factorio_path"] is None:
+    if config["factorio_data"] is None:
         window.mainButton.setText("Setup")
     else:
         window.mainButton.setText("New Project")
 
 
 def on_main_button_clicked():
-    if config["factorio_path"] is None:
+    if config["factorio_data"] is None:
         location_setup_dialog.show_dialog()
     else:
         # Handle other actions when mainButton is clicked
@@ -35,10 +35,9 @@ location_setup_dialog = LocationSetupDialog(config, update_button_text)
 window.mainButton.clicked.connect(on_main_button_clicked)
 window.showEvent = lambda event: update_button_text()
 
-window.resize(800, 600)  # Set initial window size without changing position
+window.resize(1000, 600)
 
 window.show()
 update_button_text()
-ic(config["factorio_path"])
 
 sys.exit(app.exec_())
